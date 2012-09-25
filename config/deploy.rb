2a447958ca27c0e8b45ca9dbdaad29a0fb6b7a96
David Jacobs <david@wit.io>
2012-09-25 09:55:55 -0700

    add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,166 @@
+# A Capistrano deployment file for lettersrb.com
+# by David Jacobs, (c) 2012
+
+$LOAD_PATH << "#{ENV['rvm_path']}/lib"
+
+require 'bundler/capistrano'
+require 'rvm/capistrano'
+
+# Helpers
+def run_in_current(command, env='production')
+  run_in_path(command, current_path, env)
+end
+
+def run_in_shared(command, env='production')
+  run_in_path(command, shared_path, env)
+end
+
+def run_in_path(command, path, env='production')
+  set_env = "RACK_ENV=#{env}"
+  run "cd #{path} && #{set_env} #{command}"
+end
+
+def ssh_repo(name, user=user, domain=domain)
+  "ssh://#{user}@#{domain}/home/#{user}/git/#{name}.git"
+end
+
+# Universal values
+set :user,            'david'
+set :domain,          'wit.io'
+set :application,     'letters-web'
+
+# Ruby version management
+set :rvm_type,        :user
+set :rvm_ruby_string, '1.9.2@web'
+
+role :web,            domain
+role :app,            domain
+role :db,             domain, :primary => true
+
+set :deploy_to,       "/home/#{user}/www/lettersrb.com" # FIXME
+set :deploy_via,      :remote_cache
+
+# Set the Path
+# Add bundler_stubs to PATH so we don't have to prefix
+# binary calls with the atrocious `bundle exec`
+default_environment['PATH'] = "#{current_path}/bundler_stubs:$PATH"
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+# Bundler
+# Create symlinks to Bundler-installed binaries in bundler_stubs
+set :bundle_flags,    '--deployment --quiet --binstubs=./bundler_stubs'
+
+# Version control - General
+set :use_sudo,        false
+set :scm_verbose,     false
+
+# Version control - Remote Rails application repo
+set :scm,             :git
+set :repository,      ssh_repo(application)
+set :branch,          'master'
+
+namespace :servers do
+  set :server_files, ['nginx/sites-enabled/letters-web.conf']
+  namespace :link do
+    desc 'link server config files in file system'
+    task :config do
+      source_dir = "#{current_path}/config/"
+      dest_dir = "/etc/"
+
+      server_files.each do |file|
+        source_file = source_dir + file
+        dest_file = dest_dir + file
+        sudo "ln -nfs #{source_file} #{dest_file}"
+      end
+    end
+
+    task :maintenance do
+      source_dir = "#{current_path}/public/maintenance"
+      dest_dir = "maintenance"
+      run_in_shared "ln -s #{source_dir} maintenance"
+    end
+  end
+
+  desc 'clean server config files from file system'
+  task :clean do
+    server_files.each do |file|
+      sudo "rm /etc/#{file}"
+    end
+  end
+end
+
+# Server - Unicorn
+namespace :unicorn do
+  set :unicorn_pid, 'tmp/pids/unicorn.pid'
+
+  desc "start unicorn master"
+  task :start do 
+    config = 'config/unicorn.rb'
+    run_in_current "unicorn -c #{config} -E production -D"
+  end
+
+  task :stop do 
+    run_in_current "kill -9 $(cat #{unicorn_pid})"
+  end
+
+  task :graceful_stop do
+    run_in_current "kill -s QUIT $(cat #{unicorn_pid})"
+  end
+
+  task :reload do
+    run_in_current "kill -s USR2 $(cat #{unicorn_pid})"
+  end
+
+  task :restart do
+    stop
+    start
+  end
+end
+
+# Server - Nginx
+namespace :nginx do
+  desc 'reload nginx server'
+  task :reload do
+    sudo '/etc/init.d/nginx reload'
+  end
+end
+
+# Deployment - Clear out default tasks that rely on deprecated libraries
+namespace :deploy do
+  task :start do
+    unicorn.start
+  end
+
+  task :stop do
+    unicorn.stop
+  end
+
+  task :restart do
+    unicorn.restart
+  end
+
+  task :config do
+    servers.link.config
+  end
+
+  task :migrations do; end
+  task :migrate do; end
+end
+
+# Dependencies
+# Bootstrap correctly
+before 'deploy:cold',        'deploy:setup'
+
+# Only keep latest releases
+after 'deploy',              'deploy:cleanup'
+
+# Link old server config if we rollback
+after 'rollback:default',    'servers:link:config'
+
+# Link current maintenance page
+before 'deploy:maintenance', 'servers:link:maintenance'
+
+# Link new server config if updated
+before 'deploy:config',      'deploy'
+after 'servers:link:config', 'nginx:reload'
